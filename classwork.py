fb269643cb8fe68c4c65af9dda917059410a0a5e
autumn-ragland <raglanda998@gmail.com>
2019-08-27 09:43:54 -0500

    Graded 08-27-2019 09:40:50

--- classwork.py
+++ classwork.py
@@ -16,8 +16,6 @@ def checkPassword(password1, password2):
     else:
         return False
 
-
-
 # ### Problem 3
 # Write a function that determines if a number passed to it is odd or even. Pass a number of your choosing (using input a good idea)
 # and then using the result from the function, print if the number was even or not.
@@ -41,11 +39,11 @@ def numEntered():
 # * Create a *third* function that takes a single greeting parameter (ex. ```Howdy```) and returns a string using
 # the passed in greeting and 'World' (ex. ```Howdy World!```)
 # * From your *first* function, call the function(s) and print out the final result returned
-#
 def call():
     print(f2())
 
 def f2():
+    # !! : this could easily be user input instead of hard coded 
     return f3("Hello")
 
 def f3(greeting):
@@ -55,7 +53,6 @@ def f3(greeting):
 # ### Problem 5:
 # ##### We will keep having this problem until EVERYONE gets it right without help
 # Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
-#
 def repeat():
     userInput = ''
     while userInput !='q':
@@ -63,32 +60,27 @@ def repeat():
 
 # ### Challenge
 # Create a function that accepts 2 numbers. When the function is called return the sum, difference, product, and quotient as 4 separate return values.
-#
 # Print the 4 results that are returned using f-strings.
-#
 # Example: If 2 and 6 are passed into the function, output should be similar to the following:
-#
 # ```
 # The sum of 2 + 6 is 8
 # The difference of 2 - 6 is  -4
 # The product of 2 * 6 is 12
 # The quotient of 2 / 6 is .333
 # ```
-#
-#
-#
+
 def main():
     #problem 1
-    #printNumbers()
+    printNumbers()
     #problem 2
-    # userInput1 = input("Enter a password")
-    # userInput2 = input("Please confirm your password")
-    # print(checkPassword(userInput1, userInput2))
+    userInput1 = input("Enter a password")
+    userInput2 = input("Please confirm your password")
+    print(checkPassword(userInput1, userInput2))
     #problem 3
-    # numEntered()
+    numEntered()
     #problem 4
-     call()
+    call()
     #problem 5
-    # repeat()
+    repeat()
 
 main()
